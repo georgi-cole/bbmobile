5aab89f3d0b8fd093c69910032226b3c8f6019b9
georgi-cole <georgi.val.kolev@gmail.com>
2025-09-30 00:06:17 +0300

    Update CACHE_NAME for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bb-pwa-v1';
+const CACHE_NAME = 'bb-pwa-v-intro-outro-1';
 const CORE = ['./','./index.html','./styles.css','./mobile.css','./site.webmanifest'];
 
 self.addEventListener('install', (e) => {
